6580ae0338c98e87c4561efd4b2e146068ac0218
Ilias El Abbassi <ilias.elabbassi.etu@univ-lille.fr>
2022-01-15 19:28:15 +0100

    feat: args processing

--- free-ftp/FTP_Client.py
+++ free-ftp/FTP_Client.py
@@ -3,9 +3,10 @@ import sys
 import logging
 
 class FTP_Client:
-    def __init__(self):
+    def __init__(self, host, username):
         self.PORT = 21 
-        self.HOST = "ftp.free.fr"
+        self.HOST = host
+        self.USERNAME = username
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.setupLogger()
 
@@ -20,14 +21,25 @@ class FTP_Client:
     def connect(self):
         try:
             self.socket.connect((self.HOST, self.PORT))
-            logging.info("Connection to the distant ftp server succeed {0}:{1}".format(self.HOST, self.PORT))
+            logging.info("Connection to the distant ftp server succeed {0}:{1} as {2}".format(self.HOST, self.PORT, self.USERNAME))
         except socket.error:
             logging.error("connection to the distant server failed {0}:{1}".format(self.HOST, self.PORT))
             sys.exit()
     
 
 def main():
-    ftp_client = FTP_Client()
+    if len(sys.argv) < 3:
+        print("need args : HOST USERNAME")
+        sys.exit()
+
+#    host = "ftp.free.fr"
+    host = sys.argv[1]
+    username = sys.argv[2]
+
+    ftp_client = FTP_Client(
+        host=host,
+        username=username
+    )
     ftp_client.connect()
 
 if __name__ == "__main__":
---
+++ test/args.py
@@ -0,0 +1,6 @@
+import sys
+
+print(len(sys.argv))
+
+for ele in sys.argv:
+    print(ele)
