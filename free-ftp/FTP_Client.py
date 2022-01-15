1ac0fe178b980e229183996c083d4f2ec95845b3
Ilias El Abbassi <ilias.elabbassi.etu@univ-lille.fr>
2022-01-15 20:29:47 +0100

    feat: read message from ftp server, redo connection, begin LIST and HELP

--- free-ftp/FTP_Client.py
+++ free-ftp/FTP_Client.py
@@ -1,6 +1,12 @@
+from distutils.log import error
+from queue import Empty
+from re import S
 import socket
 import sys
 import logging
+
+BUFFER_SIZE = 1024 # standard buffer size
+FORMAT = "utf-8"
 
 class FTP_Client:
     def __init__(self, host, username):
@@ -18,6 +24,21 @@ class FTP_Client:
             format='%(asctime)s:%(levelname)s:%(message)s'
             )
 
+    def getMsg(self):
+        try:
+            return self.socket.recv(BUFFER_SIZE).decode(FORMAT)
+        except:
+            logging.error("in getMsg :")
+    
+    def read(self):
+        buffer = ""
+        data = self.getMsg()
+        while data:
+            buffer+=data
+            data=self.getMsg
+        
+        return buffer
+
     def connect(self):
         try:
             self.socket.connect((self.HOST, self.PORT))
@@ -25,22 +46,72 @@ class FTP_Client:
         except socket.error:
             logging.error("connection to the distant server failed {0}:{1}".format(self.HOST, self.PORT))
             sys.exit()
+
+        try:
+            self.socket.send("AUTH".encode(FORMAT))
+            print(self.getMsg())
+        except:
+            logging.error("in CONNECT : ")
+
+    """
+    LIST: Show information of a specific file/folder or current folder
+
+    this method will send a LIST commande to the FTP server and process the data received acordingly:
+        -
+        -
+        -
+    """
+    def list_files(self):
+        logging.info("Starting list_files() method...")
+
+        try:
+            # send LIST cmd to the ftp server
+            self.socket.send("LIST".encode(FORMAT))
+            print(self.getMsg())
+        except:
+            logging.error("Couldn't make the request")
+            return
     
+    def HELP(self):
+        logging.info("Starting HELP method...")
+
+        try:
+            # send LIST cmd to the ftp server
+            self.socket.send("HELP\r\n".encode(FORMAT))
+            print(self.getMsg())
+        except:
+            logging.error("Couldn't make the request")
+            return
+
+        try:
+            reply = ""
+            while True:
+                temp = self.socket.recv(BUFFER_SIZE)
+                if temp:
+                    reply += temp
+                else:
+                    break
+            print(reply)
+        except NameError:
+            logging.error("in HELP: {0}".format(NameError))
+            return
+
 
 def main():
     if len(sys.argv) < 3:
         print("need args : HOST USERNAME")
         sys.exit()
 
-#    host = "ftp.free.fr"
+    #host = "ftp.free.fr"
     host = sys.argv[1]
     username = sys.argv[2]
 
     ftp_client = FTP_Client(
-        host=host,
+        host=host,  
         username=username
     )
     ftp_client.connect()
+    ftp_client.HELP()
 
 if __name__ == "__main__":
     main()
